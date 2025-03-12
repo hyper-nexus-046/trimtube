9554775b4474461c6f80cbe31337e15cfd6b2474
Junaid Maqbool <lucifermjm@gmail.com>
2025-03-12 18:11:18 +0500

    Day Two

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-export default function HomePage() {
+import Header from "~/app/_components/common/header";
+import { currentUser } from "@clerk/nextjs/server";
+
+export default async function HomePage() {
+  const user = await currentUser();
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          TrimTube
-        </h1>
+      <Header />
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold">Welcome to Trimtube</h1>
+        <p className="text-lg">You are logged in as {JSON.stringify(user)}</p>
       </div>
     </main>
   );
