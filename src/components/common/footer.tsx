a4f34bda15c0c730d120182d6e718ec3856ee75e
Malay Khan <malaykhan900@gmail.com>
2025-03-20 19:13:53 +0500

    [Bottom cta]-done

---
+++ src/components/common/footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum quam
+      voluptas aut eligendi omnis ducimus, ipsa rerum esse minus commodi eius
+      eum natus doloremque nesciunt labore sunt? Exercitationem, fugiat facilis!
+    </div>
+  );
+};
