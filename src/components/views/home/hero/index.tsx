810601d72f6a19c254aabf06ca53ecc5e8a83e9d
Malay Khan <malaykhan900@gmail.com>
2025-04-03 12:44:11 +0500

    [Small task]-completed

--- src/components/views/home/hero/index.tsx
+++ src/components/views/home/hero/index.tsx
@@ -7,17 +7,15 @@ export const Hero = () => {
       title='The Fast and Modern way to download YouTube videos.'
       para='Elevate your Youtube video editing with the speed and precision of Trimtube'
       sectionName='Hero'
-      className='mb-content-lg pt-content'
+      className='mb-content-lg pt-content flex flex-col items-center'
       headingLevel='h1'
       isMax
     >
-      <div className='flex justify-center'>
-        <div className='w-3/4 lg:w-2/4 xl:w-2/5 rounded-md p-1 bg-[length:400%_400%] animate-[gradient-x_6s_ease_infinite] bg-[linear-gradient(90deg,#26091a_0%,#c65a58_30%,#9f4e94_50%,#7942cf_70%,#0e031e_100%)]'>
-          <Input
-            placeholder='Enter your Youtube video link here or just copy and paste it'
-            className='bg-black/60 px-4 py-5 sm:py-[26px] rounded-md text-xs border-none text-white'
-          />
-        </div>
+      <div className='w-3/4 lg:w-2/4 xl:w-2/5 rounded-md p-1 bg-[length:400%_400%] animate-[gradient-x_6s_ease_infinite] bg-[linear-gradient(90deg,#26091a_0%,#c65a58_30%,#9f4e94_50%,#7942cf_70%,#0e031e_100%)]'>
+        <Input
+          placeholder='Enter your Youtube video link here or just copy and paste it'
+          className='bg-black/60 px-4 py-5 sm:py-[26px] rounded-md text-xs border-none text-white'
+        />
       </div>
     </TitledWrapper>
   )
