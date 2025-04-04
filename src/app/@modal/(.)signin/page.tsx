1ddc59b31ed4ea43074ac17b2476b1074e9387db
Malay Khan <malaykhan900@gmail.com>
2025-04-04 14:34:33 +0500

    [Signin and Signup dailog]-done

--- src/app/@modal/(.)signin/page.tsx
+++ src/app/@modal/(.)signin/page.tsx
@@ -1,3 +1,7 @@
+import Image from 'next/image'
+import dashboard from 'public/common/dashboard.png'
+
+import { Logo } from '~/components/common/header/logo'
 import { LoginForm } from '~/components/common/login-form'
 
 import { Modal } from './modal'
@@ -5,7 +9,45 @@ import { Modal } from './modal'
 export default async function SignInModal() {
   return (
     <Modal>
-      <LoginForm />
+      <div className='grid lg:grid-cols-[1fr_1.5fr] items-center min-h-screen bg-[#02020e]'>
+        <div className='relative h-full flex flex-col justify-center'>
+          <Logo className='absolute top-3 left-5' />
+          <LoginForm className='w-full md:w-[90%] lg:w-4/5 mx-auto  px-12 lg:px-0' />
+        </div>
+        <div
+          style={{
+            background: `
+            linear-gradient(to bottom right,
+              #1f2537 0%,
+              #1f2537 20%,
+              #242a3c 20%,
+              #242a3c 40%,
+              #2a2d41 40%,
+              #2a2d41 60%,
+              #3a3e4f 60%,
+              #3a3e4f 80%,
+              #3a3e4f 100%
+            )
+          `
+          }}
+          className='relative hidden lg:flex flex-col p-6 lg:p-10 overflow-hidden h-full'
+        >
+          <div className='pt-4 space-y-3'>
+            <h1 className='text-heading-h5 text-semibold'>
+              Design for individuals
+            </h1>
+            <p className='w-3/4'>
+              See the analytics and grow your date for Task remotely, from
+              anywhere!
+            </p>
+          </div>
+          <Image
+            className='-rotate-12 absolute -bottom-10 xl:-bottom-16 -right-24 xl:-right-36 rounded-xl'
+            src={dashboard}
+            alt='dashboard'
+          />
+        </div>
+      </div>
     </Modal>
   )
 }
--- src/app/@modal/(.)signup/modal.tsx
+++ src/app/@modal/(.)signup/modal.tsx
@@ -2,13 +2,7 @@
 
 import { useRouter } from 'next/navigation'
 
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle
-} from '~/components/ui/dialog'
+import { Dialog, DialogContent } from '~/components/ui/dialog'
 
 export function Modal({ children }: { children: React.ReactNode }) {
   const router = useRouter()
@@ -19,23 +13,8 @@ export function Modal({ children }: { children: React.ReactNode }) {
 
   return (
     <Dialog defaultOpen={true} open={true} onOpenChange={onDismiss}>
-      <DialogContent className='max-w-[75%] p-5'>
-        <DialogHeader className='space-y-2'>
-          <DialogTitle className='sr-only text-heading-h7 font-bold'>
-            Login
-          </DialogTitle>
-          <DialogDescription className='sr-only text-sm text-secondary-foreground'>
-            asdfasdf
-          </DialogDescription>
-        </DialogHeader>
-
-        <div className='grid lg:grid-cols-2'>
-          <div className='flex flex-col gap-4 p-6 md:p-10'>
-            <div className='flex flex-1 items-center justify-center'>
-              <div className='w-full max-w-xs'>{children}</div>
-            </div>
-          </div>
-        </div>
+      <DialogContent className='lg:max-w-[90%] xl:max-w-[75%] my-96 overflow-hidden p-0 lg:[&>button.absolute]:hidden'>
+        {children}
       </DialogContent>
     </Dialog>
   )
