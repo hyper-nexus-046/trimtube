f7dfab7106358322517ce51313047fd4b9dc09d2
Junaid Maqbool <lucifermjm@gmail.com>
2025-04-11 21:30:16 +0500

    [Small Fixes Reviewed & Extra Things removed]

--- src/app/(root)/pricing/page.tsx
+++ src/app/(root)/pricing/page.tsx
@@ -1,7 +1,6 @@
 import data from '~/content/json-files/faq.json'
 
 import { Faq } from '~/components/common/faq'
-import { PricingModal } from '~/components/common/modals/pricing-modal'
 import { Features } from '~/components/views/home/features'
 import { Pricing } from '~/components/views/home/pricing'
 import { PageWrapper } from '~/components/wrappers'
@@ -9,7 +8,6 @@ import { PageWrapper } from '~/components/wrappers'
 export default function Page() {
   return (
     <PageWrapper>
-      <PricingModal />
       <Pricing headingLevel='h1' className='mt-content-sm' />
       <Features />
       <Faq data={data} />
